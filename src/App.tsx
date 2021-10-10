e276918ae7be4f7efa302533091bd5f5d77a5bf6
KostenvichDenis <kostenevichdenis@yandex.ru>
2021-10-10 13:31:51 +0200

    ready for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import Navbar from './conmponents/Navbar';
+import AboutPage from './pages/AboutPage';
+import TodosPage from './pages/TodosPage';
+import { Switch, Route } from 'react-router-dom'
 
-function App() {
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact render={() => <TodosPage />} />
+        <Route path="/about" exact component={AboutPage} />
+      </Switch>
+    </Fragment>
   );
 }
 
 export default App;
+
+
